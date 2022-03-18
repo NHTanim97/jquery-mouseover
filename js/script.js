cb820613aa770cca2f9a611e0f45c685f3cafef9
NHTanim97 <101093767+NHTanim97@users.noreply.github.com>
2022-03-18 12:06:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ 
+    $(".hide").mouseover(function(){
+      $(".box").hide();
+    //   5000 mili sec = 5 sec;
+    });
+
+    $('.show').mouseover(function(){
+        $('.box').show();
+    });
+
+});
